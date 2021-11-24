7a5fa8519cb47aefc3d08334c144ba1dd54e6114
kantraman <93331484+kantraman@users.noreply.github.com>
2021-11-24 19:20:22 +0530

    Line through for completed tasks

--- scripts/toDoList.js
+++ scripts/toDoList.js
@@ -18,11 +18,13 @@ function getToDoList() {
             strOutput += "<table class='table-sm'>";
             arrResponse.forEach(item => {
                 strOutput += "<tr><td>";
-                if (item.completed === false)
+                if (item.completed === false) {
                     strOutput += `<input id ='chk${i}' type='checkbox' class='form-check-input' onchange='chkBoxChecked(this)'/></td><td>`;
-                else
-                    strOutput += `<input id ='chk${i}' type='checkbox' class='form-check-input' checked disabled/></td><td style='color:red;text-decoration: line-through;'>`;
-                strOutput += `<label for = 'chk${i}'>${item.title}</label></td></tr>`
+                    strOutput += `<label for = 'chk${i}'>${item.title}</label></td></tr>`
+                } else {
+                    strOutput += `<input id ='chk${i}' type='checkbox' class='form-check-input' checked disabled/></td><td>`;
+                    strOutput += `<label for = 'chk${i}' class='text-decoration-line-through text-danger' >${item.title}</label></td></tr>`
+                }
                 i += 1;
             });
             strOutput += "</table>"
@@ -40,11 +42,11 @@ function logOut() {
 }
 
 function chkBoxChecked(obj) {
-    let label = obj.parentNode.nextSibling;
+    let label = obj.parentNode.nextSibling.firstChild;
     if (obj.checked) {
         numChk += 1;
         label.style.textDecoration = "line-through";
-        label.style.color = "red";
+        label.style.color = "#dc2545";
         if (numChk === 5) {
             alert(" “Congrats!!! 5 Tasks have been Successfully Completed.");
             numChk = 0;
@@ -54,7 +56,7 @@ function chkBoxChecked(obj) {
             numChk -= 1;
         else
             numChk = 4;
-        label.style.textDecoration = "";
+        label.style.textDecoration = "none";
         label.style.color = "black";
     }
 }
